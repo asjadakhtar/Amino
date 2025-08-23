2f8f15846a479b4e61b6c6d9c197bfd4dd4a0a5f
asjadakhtar <asjadtech203@gmail.com>
2025-08-23 18:16:16 +0500

    #

---
+++ template-parts/header/main.php
@@ -0,0 +1,30 @@
+<header class="w-full bg-gradient-to-r from-black to-[#4a2e1c]">
+  <div class="container mx-auto flex justify-between items-center p-5 text-white">
+      
+      <!-- Logo -->
+      <div class="w-10">
+          <!-- Aap yahan apni company ka SVG logo istemal kar sakte hain -->
+          <svg viewBox="0 0 227 222" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M128.534 83.2519H29.1311V0.985596H128.534C175.46 0.985596 213.904 37.893 213.904 83.2519C213.904 128.611 175.46 165.518 128.534 165.518H86.2731V221.014H29.1311V83.2519H128.534Z" fill="white"/>
+          </svg>
+      </div>
+
+      <!-- Navigation Links -->
+      <nav class="hidden md:flex">
+          <ul class="flex items-center space-x-10 text-base font-light text-gray-200">
+              <li><a href="#" class="hover:text-white transition-colors">How it works</a></li>
+              <li><a href="#" class="hover:text-white transition-colors">Techstack</a></li>
+              <li><a href="#" class="hover:text-white transition-colors">Pricings</a></li>
+              <li><a href="#" class="hover:text-white transition-colors">FAQs</a></li>
+          </ul>
+      </nav>
+
+      <!-- CTA Button -->
+      <div>
+          <a href="#" class="border border-gray-400 rounded-md px-7 py-3 text-base hover:bg-white hover:text-black transition-colors duration-300">
+              Book a call
+          </a>
+      </div>
+
+  </div>
+</header>
