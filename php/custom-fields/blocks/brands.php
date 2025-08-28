c2ff64eb62df2f97c9051a751406d1f0d3cf5bb7
asjadakhtar <asjadtech203@gmail.com>
2025-08-28 17:49:43 +0500

    #

--- php/custom-fields/blocks/brands.php
+++ php/custom-fields/blocks/brands.php
@@ -12,42 +12,42 @@
                 
                 <!-- Original Slides -->
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Logo_1_.png" alt="HelpBnk" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo_1_.png" alt="HelpBnk" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Group-14167.png" alt="Methodic" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="Methodic" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Group-213.png" alt="Clarity AI" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-213.png" alt="Clarity AI" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                        <img src="http://localhost/amino/wp-content/uploads/2025/08/Logo-1.png" alt="Refermarket" class="brand-logo">
+                        <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo-1.png" alt="Refermarket" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Group-213.png" alt="LLM Observer" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-213.png" alt="LLM Observer" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Logo_1_.png" alt="Winning Hunter" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo_1_.png" alt="Winning Hunter" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Group-14167.png" alt="Luxe Designs" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="Luxe Designs" class="brand-logo">
                 </div>
                 
                 <!-- Duplicate slides for the seamless loop -->
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Logo_1_.png" alt="HelpBnk" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo_1_.png" alt="HelpBnk" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Group-14167.png" alt="Methodic" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="Methodic" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Group-213.png" alt="Clarity AI" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-213.png" alt="Clarity AI" class="brand-logo">
                 </div>
                     <div class="swiper-slide !w-auto">
-                        <img src="http://localhost/amino/wp-content/uploads/2025/08/Logo-1.png" alt="Refermarket" class="brand-logo">
+                        <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo-1.png" alt="Refermarket" class="brand-logo">
                 </div>
                 <div class="swiper-slide !w-auto">
-                    <img src="http://localhost/amino/wp-content/uploads/2025/08/Group-14167.png" alt="LLM Observer" class="brand-logo">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="LLM Observer" class="brand-logo">
                 </div>
             </div>
         </div>
--- php/custom-fields/blocks/hero.php
+++ php/custom-fields/blocks/hero.php
@@ -7,7 +7,7 @@
         loop
         muted
         playsinline>
-        <source src="http://localhost/amino/wp-content/uploads/2025/08/istockphoto-619025058-640_adpp_is.mp4" type="video/mp4">
+        <source src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/istockphoto-619025058-640_adpp_is.mp4" type="video/mp4">
         Your browser does not support the video tag.
     </video>
 
