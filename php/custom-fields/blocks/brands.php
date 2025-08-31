e80ce5d041e89ebb60146a115b247127cab13dc8
asjadakhtar <asjadtech203@gmail.com>
2025-08-31 16:09:02 +0500

    #

--- php/custom-fields/blocks/brands.php
+++ php/custom-fields/blocks/brands.php
@@ -32,8 +32,44 @@
                 <div class="swiper-slide !w-auto">
                     <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="Luxe Designs" class="brand-logo">
                 </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo_1_.png" alt="HelpBnk" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="Methodic" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-213.png" alt="Clarity AI" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                        <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo-1.png" alt="Refermarket" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-213.png" alt="LLM Observer" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo_1_.png" alt="Winning Hunter" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="Luxe Designs" class="brand-logo">
+                </div>
                 
                 <!-- Duplicate slides for the seamless loop -->
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo_1_.png" alt="HelpBnk" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="Methodic" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-213.png" alt="Clarity AI" class="brand-logo">
+                </div>
+                    <div class="swiper-slide !w-auto">
+                        <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo-1.png" alt="Refermarket" class="brand-logo">
+                </div>
+                <div class="swiper-slide !w-auto">
+                    <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Group-14167.png" alt="LLM Observer" class="brand-logo">
+                </div>
                 <div class="swiper-slide !w-auto">
                     <img src="https://beige-grasshopper-864950.hostingersite.com/wp-content/uploads/2025/08/Logo_1_.png" alt="HelpBnk" class="brand-logo">
                 </div>
