df4c8cb56ee51fc0d34897ded6870568265d1d39
asjadakhtar <asjadtech203@gmail.com>
2025-10-20 16:39:50 +0500

    #

--- php/custom-fields/blocks/calendly.php
+++ php/custom-fields/blocks/calendly.php
@@ -1,5 +1,18 @@
 <section class="py-12 md:py-24 my-0 md:my-24">
   <div class="max-w-4xl mx-auto px-4">
-    <div class="calendly-inline-widget" data-url="https://calendly.com/aminomarketing-info/business-consultation?primary_color=ff5c07"></div>
+    <?php 
+      $calendly_link = get_field('calendly_link');
+      if( $calendly_link ): ?>
+        <div class="calendly-inline-widget" 
+             data-url="<?php echo esc_url($calendly_link); ?>" 
+             style="min-width:320px;height:750px;">
+        </div>
+        <script type="text/javascript" 
+                src="https://assets.calendly.com/assets/external/widget.js" 
+                async>
+        </script>
+    <?php else: ?>
+        <p class="text-center text-gray-500">Calendly link not set. Please add it in ACF.</p>
+    <?php endif; ?>
   </div>
 </section>
