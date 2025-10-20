7c37f76eb6e4c955319fecfe0a3bea48147d2df3
asjadakhtar <asjadtech203@gmail.com>
2025-10-20 16:37:14 +0500

    #

---
+++ php/custom-fields/blocks/calendly.php
@@ -0,0 +1,5 @@
+<section class="py-12 md:py-24 my-0 md:my-24">
+  <div class="max-w-4xl mx-auto px-4">
+    <div class="calendly-inline-widget" data-url="https://calendly.com/aminomarketing-info/business-consultation?primary_color=ff5c07"></div>
+  </div>
+</section>
