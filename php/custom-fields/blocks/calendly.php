f42b7bc84e540ef16d68b9576dc856772523b68d
asjadakhtar <asjadtech203@gmail.com>
2025-10-20 16:50:59 +0500

    #

--- php/custom-fields/blocks/calendly.php
+++ php/custom-fields/blocks/calendly.php
@@ -1,3 +1,8 @@
+<?php
+/* Template Name: Calendly Page */
+get_header();
+?>
+
 <section class="py-12 md:py-24 my-0 md:my-24">
   <div class="max-w-4xl mx-auto px-4">
     <?php 
@@ -7,12 +12,10 @@
              data-url="<?php echo esc_url($calendly_link); ?>" 
              style="min-width:320px;height:750px;">
         </div>
-        <script type="text/javascript" 
-                src="https://assets.calendly.com/assets/external/widget.js" 
-                async>
-        </script>
     <?php else: ?>
         <p class="text-center text-gray-500">Calendly link not set. Please add it in ACF.</p>
     <?php endif; ?>
   </div>
 </section>
+
+<?php get_footer(); ?>
