262463087b9a311c5d4e358215f2eef886522ec9
asjadakhtar <asjadtech203@gmail.com>
2025-10-29 15:39:51 +0500

    #

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -57,7 +57,7 @@
             </a>
 
             <a href="https://www.instagram.com/amino_marketing/" target="_blank" rel="noopener noreferrer" class="hover:text-[#ff520e] transition-colors duration-300">
-                <svg fill="#000000" class="w-6 h-6" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                <svg fill="#fff" class="w-6 h-6" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
                     viewBox="0 0 32 32" xml:space="preserve">
                     <path d="M23,0H9C4,0,0,4,0,9v6v8c0,5,4,9,9,9h14c5,0,9-4,9-9v-8V9C32,4,28,0,23,0z M16,10c3.3,0,6,2.7,6,6s-2.7,6-6,6s-6-2.7-6-6
                     S12.7,10,16,10z M22,7c0-1.1,0.9-2,2-2s2,0.9,2,2s-0.9,2-2,2S22,8.1,22,7z"/>
