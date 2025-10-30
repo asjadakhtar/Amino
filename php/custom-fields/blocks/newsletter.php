69fe5b2ac79d25bcbb2e4fd24e74f1cdbcbadb60
asjadakhtar <asjadtech203@gmail.com>
2025-10-30 20:05:37 +0500

    #

--- php/custom-fields/blocks/newsletter.php
+++ php/custom-fields/blocks/newsletter.php
@@ -1,21 +1,25 @@
-<section class="relative py-16 md:py-24">
-    <div class="max-w-3xl mx-auto text-center px-4">
-        
-        <!-- Form Wrapper -->
-        <div class="relative bg-[#ffffff12] border border-white/5 rounded-xl p-6 md:p-8 newsletter-hover-wrapper">
-            <!-- Heading -->
-            <h2 class="text-4xl sm:text-4xl font-normal text-white mb-4">
-                Stay Ahead in the Peptide Industry
-            </h2>
-            <p class="text-[#D6D6D6] text-base sm:text-lg mb-10">
-                Subscribe to get marketing insights, compliance tips, and strategies for peptide brand growth-straight to your inbox.
-            </p>
-            <div class="absolute bottom-[-45px] right-[-233px] h-[126px] w-[621px] bg-[#ff520e] opacity-0 blur-[100px] transition-opacity duration-500 ease-in-out z-10 newsletter-glow" aria-hidden="true"></div>
+<section class="relative py-16 md:py-24 overflow-hidden">
+  <div class="max-w-3xl mx-auto text-center px-4">
+    <!-- Form Wrapper -->
+    <div class="relative bg-[#ffffff12] border border-white/5 rounded-xl p-6 md:p-8 newsletter-hover-wrapper overflow-hidden">
+      <!-- Heading -->
+      <h2 class="text-4xl sm:text-4xl font-normal text-white mb-4">
+        Stay Ahead in the Peptide Industry
+      </h2>
+      <p class="text-[#D6D6D6] text-base sm:text-lg mb-10">
+        Subscribe to get marketing insights, compliance tips, and strategies for peptide brand growth-straight to your inbox.
+      </p>
 
-            <div class="relative z-20 newsletter-flex-wrapper">
-                <?php echo do_shortcode('[contact-form-7 id="3d1b9cd" title="Newsletter"]'); ?>
-            </div>
-        </div>
+      <!-- Orange Glow -->
+      <div
+        class="absolute bottom-[-45px] right-[-233px] h-[126px] w-[621px] bg-[#ff520e] opacity-0 blur-[100px] transition-opacity duration-500 ease-in-out z-10 newsletter-glow pointer-events-none"
+        aria-hidden="true">
+      </div>
 
+      <!-- Form -->
+      <div class="relative z-20 newsletter-flex-wrapper">
+        <?php echo do_shortcode('[contact-form-7 id="3d1b9cd" title="Newsletter"]'); ?>
+      </div>
     </div>
+  </div>
 </section>
